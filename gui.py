cd3e809035b8c402b61d96b58378d7a456a53f29
csgellert <csapodi.gellert@gmail.com>
2021-04-17 17:39:39 +0200

    add positioning

--- gui.py
+++ gui.py
@@ -2,20 +2,30 @@ import tkinter as tk
 from PIL import Image, ImageTk
 
 RADIUS = 20
-
 class Room():
     def __init__(self,tlx,tly,brx,bry):
-        self.tlx=tlx
+        self.tlx=tlx #top left corner
         self.tly=tly
         self.brx=brx
         self.bry=bry
+        self.center = ((tlx+brx)/2 , (tly+bry)/2)
 
 def move(e):
+    global isMoving
     pos = canvas.coords(circle)
     if e.x>=pos[0] and e.x<=pos[2] and e.y>=pos[1] and e.y <=pos[3]:
         my_label.config(text=f"Coordinates: x: {e.x} y: {e.y}")
         canvas.coords(circle,e.x-RADIUS,e.y-RADIUS,e.x+RADIUS,e.y+RADIUS)
-
+        isMoving = True
+def release(e):
+    global isMoving
+    global rooms
+    if isMoving:
+        #print(e)
+        for i in rooms:
+            if e.x>=i.tlx and e.x<=i.brx and e.y>=i.tly and e.y <=i.bry:
+                canvas.coords(circle,i.center[0]-RADIUS, i.center[1]-RADIUS,i.center[0]+RADIUS, i.center[1]+RADIUS)
+        isMoving = False
 
 
 if __name__ == '__main__':
@@ -23,7 +33,7 @@ if __name__ == '__main__':
     root.title("Próba GUI")
     root.geometry("1000x600")
     root.minsize(width=200, height=200)
-    
+    isMoving = False
     canvas = tk.Canvas(width = 1000, height=550, bg='white')
     canvas.pack(expand=tk.YES)
     img = Image.open('OpenOffice.png')
@@ -35,7 +45,12 @@ if __name__ == '__main__':
     #canvas.create_rectangle(0,0,250,170,fill="blue")
     my_label = tk.Label(root,text="")
     my_label.pack()
+    
     canvas.bind('<B1-Motion>',move)
-
+    canvas.bind('<ButtonRelease-1>',release)
+    rooms = []
     room1 = Room(0,0,250,170)
+    room2 = Room(246,15,400,160)
+    rooms.append(room1)
+    rooms.append(room2)
     root.mainloop()
